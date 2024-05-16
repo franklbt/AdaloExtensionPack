4a4a5a779d06d55e0c0566c403bdd9e2d0aced32
Frank <frank.libolt@hotmail.com>
2024-05-16 22:16:56 +0200

    fix interface

--- AdaloExtensionPack.Core/Adalo/IAdaloTableService.cs
+++ AdaloExtensionPack.Core/Adalo/IAdaloTableService.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 
 namespace AdaloExtensionPack.Core.Adalo
 {
-    public interface IAdaloTableService<T> where T: AdaloEntity
+    public interface IAdaloTableService<T> where T : AdaloEntity
     {
         Task<List<T>> GetAllAsync((Expression<Func<T, object>> Predicate, object Value)? predicate = null);
         Task<T> PostAsync(T payload);
         Task<T> GetAsync(int recordId);
         Task DeleteAsync(int recordId);
         Task<T> PutAsync(int recordId, T payload);
+        Task<T> PutAsync(int recordId, Action<T> update);
     }
 }
